b6f40c081574d4141b9ceab9d37225701ebcd693
xB0SsZz <a93095@alunos.uminho.pt>
2022-04-23 17:40:31 +0100

    grid populated correctly

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 import sys
 import pygame
 from random import sample
+import random
 
 
 base  = 3
 side  = base*base
+width = 550
+background_color = (135, 218, 149)
+grid_element_color = (0, 0, 0)
+
 
 # randomize rows, columns and numbers (of valid base pattern)
 def shuffle(s): 
@@ -26,8 +31,55 @@ def main():
 
     for line in board: print(line)
 
+    #DRAWING THE GRID
+    pygame.init()
+    win = pygame.display.set_mode((width, width))
+    pygame.display.set_caption("Sudoku Rústico")
+    win.fill(background_color)
+    
+
+    for i in range(0, 10):
+        if i % 3 == 0:
+            pygame.draw.line(win, (0, 0, 0), (50 + 50*i, 50), (50 + 50*i, 500), 4)
+            pygame.draw.line(win, (0, 0, 0), (50, 50 + 50*i), (500, 50 + 50*i), 4)
+        pygame.draw.line(win, (0, 0, 0), (50 + 50*i, 50), (50 + 50*i, 500), 2)
+        pygame.draw.line(win, (0, 0, 0), (50, 50 + 50*i), (500, 50 + 50*i), 2)
+
+    
+
+    # POPULATING THE GRID
+
+    # choosing what numbers are actually being displayed in the beginning
+    rand_coords = []        
+    while len(rand_coords) < 17:
+        x = random.randint(0, 8)
+        y = random.randint(0, 8)
+        if (x, y) not in rand_coords:
+            rand_coords.append((x, y))
+
+    for x, y in rand_coords:
+        print(x, y)
+        print((str(board[x][y])))
 
 
+    myfont = pygame.font.SysFont('Comic Sans MS', 35)
+    for i in range(0, len(board[0])):
+        for j in range(0, len(board[0])):
+            for x, y in rand_coords:
+                if x == i and y == j:
+                    if(0 < board[i][j] < 10):
+                        value = myfont.render(str(board[i][j]), True, grid_element_color)
+                        win.blit(value, ((j+1)*50 + 15, (i+1)*50 ))
+
+    pygame.display.update()
+
+
+    
+    while True:
+        for e in pygame.event.get():
+            if e.type == pygame.QUIT:
+                pygame.quit()
+                return
 
     
 
