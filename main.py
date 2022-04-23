148b99299301bb2632c5e0a6e7a53f2945b3f30d
xB0SsZz <a93095@alunos.uminho.pt>
2022-04-23 23:19:53 +0100

    square highlighting is ok

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from asyncio.windows_events import NULL
 from cmath import rect
 from msvcrt import kbhit
 import sys
@@ -14,6 +15,9 @@ background_color = (135, 218, 149)
 grid_element_color = (0, 0, 0)
 inserted_elem_color = (0, 0, 255)
 
+# grid that will be shown in the screen
+display_grid = [ [0 for c in range(0, 9)] for r in range(0, 9) ]
+
 
 # randomize rows, columns and numbers (of valid base pattern)
 def shuffle(s): 
@@ -23,24 +27,30 @@ def shuffle(s):
 def pattern(r,c): 
     return (base*(r%base)+r//base+c)%side
 
-def main():
-    # SUDOKU SOLUTION BOARD
-    rBase = range(base) 
-    rows  = [ g*base + r for g in shuffle(rBase) for r in shuffle(rBase) ] 
-    cols  = [ g*base + c for g in shuffle(rBase) for c in shuffle(rBase) ]
-    nums  = shuffle(range(1,base*base+1))
-
-    # produce board using randomized baseline pattern
-    board = [ [nums[pattern(r,c)] for c in cols] for r in rows ]
-
-    for line in board: print(line)
-
-    #DRAWING THE GRID
-    pygame.init()
-    win = pygame.display.set_mode((width, width))
-    pygame.display.set_caption("Sudoku Rústico")
+def updateGrid(win, rectangle):
     win.fill(background_color)
-    
+
+    for i in range(0, 10):
+        if i % 3 == 0:
+            pygame.draw.line(win, (0, 0, 0), (50 + 50*i, 50), (50 + 50*i, 500), 4)
+            pygame.draw.line(win, (0, 0, 0), (50, 50 + 50*i), (500, 50 + 50*i), 4)
+        pygame.draw.line(win, (0, 0, 0), (50 + 50*i, 50), (50 + 50*i, 500), 2)
+        pygame.draw.line(win, (0, 0, 0), (50, 50 + 50*i), (500, 50 + 50*i), 2)
+
+    pygame.draw.rect(win, (33, 232, 67), rectangle)
+
+    myfont = pygame.font.SysFont('Comic Sans MS', 35)
+    for i in range(0, len(display_grid[0])):
+        for j in range(0, len(display_grid[0])):
+            if(0 < display_grid[i][j] < 10):
+                value = myfont.render(str(display_grid[i][j]), True, grid_element_color)
+                win.blit(value, ((j+1)*50 + 15, (i+1)*50 ))
+
+    pygame.display.update()
+
+def drawGrid(board, win):
+    # DRAWING THE GRID
+    win.fill(background_color)
 
     for i in range(0, 10):
         if i % 3 == 0:
@@ -61,40 +71,57 @@ def main():
         if (x, y) not in rand_coords:
             rand_coords.append((x, y))
 
+    
     for x, y in rand_coords:
-        print(x, y)
-        print((str(board[x][y])))
+        display_grid[x][y] = board[x][y]
+        print(x, y)    # LOGTEST
+        print((str(board[x][y])))  # LOGTEST
 
+    for line in display_grid:  # LOGTEST
+        print(line)
 
     myfont = pygame.font.SysFont('Comic Sans MS', 35)
-    for i in range(0, len(board[0])):
-        for j in range(0, len(board[0])):
-            for x, y in rand_coords:
-                if x == i and y == j:
-                    if(0 < board[i][j] < 10):
-                        value = myfont.render(str(board[i][j]), True, grid_element_color)
-                        win.blit(value, ((j+1)*50 + 15, (i+1)*50 ))
+    for i in range(0, len(display_grid[0])):
+        for j in range(0, len(display_grid[0])):
+            if(0 < display_grid[i][j] < 10):
+                value = myfont.render(str(display_grid[i][j]), True, grid_element_color)
+                win.blit(value, ((j+1)*50 + 15, (i+1)*50 ))
 
     pygame.display.update()
 
+def main():
+    # SUDOKU SOLUTION BOARD
+    rBase = range(base) 
+    rows  = [ g*base + r for g in shuffle(rBase) for r in shuffle(rBase) ] 
+    cols  = [ g*base + c for g in shuffle(rBase) for c in shuffle(rBase) ]
+    nums  = shuffle(range(1,base*base+1))
 
+    # produce board using randomized baseline pattern (SOLUTION)
+    board = [ [nums[pattern(r,c)] for c in cols] for r in rows ]
     
+
+    for line in board: print(line)
+
+    pygame.init()
+    win = pygame.display.set_mode((width, width))
+    pygame.display.set_caption("Sudoku Rústico")    
+    drawGrid(board, win)
+
     while True:
         for e in pygame.event.get():
             ind_x, ind_y = 0, 0
-            flag = False
             if e.type == pygame.QUIT:
                 pygame.quit()
                 return
             if e.type == pygame.MOUSEBUTTONUP:
+                # MOUSE POSITION
                 x, y = pygame.mouse.get_pos()
                 ind_x, ind_y = int(x / 50 - 1), int(y / 50 - 1)
-                print(x, y)
-                print(ind_x, ind_y)
-                # value = myfont.render(str(board[ind_y][ind_x]), True, inserted_elem_color)
-                pygame.draw.rect(win, (33, 232, 67), pygame.Rect(((ind_x + 1) * 50), ((ind_y + 1) * 50), 48, 48))
-                #win.blit(value, ((ind_x+1)*50 + 15, (ind_y+1)*50 ))
+                rectangle = pygame.Rect(((ind_x + 1) * 50), ((ind_y + 1) * 50), 48, 48)
+                # pygame.draw.rect(win, (33, 232, 67), rectangle)
+                updateGrid(win, rectangle)
                 pygame.display.update()
+                
             if e.type == pygame.KEYDOWN:
                 print("A key has been pressed")
                 
